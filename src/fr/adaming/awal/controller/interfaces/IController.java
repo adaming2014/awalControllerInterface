aefac9f3f5e94c4f4212c3fcf8c150fb89741a0a
bianloic <bian.loic@gmail.com>
2014-09-30 15:03:58 +0200

    add Controller class, Controller client class, Controller device class, Controller reseller class and Controller user class.

---
+++ src/fr/adaming/awal/controller/interfaces/IController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.controller.interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author INTI0217
+ */
+public interface IController<T> {
+
+    List<T> getAll();
+
+    boolean create(T address);
+
+    boolean update(T address);
+
+    boolean delete(T address);
+
+    boolean delete(int id);
+
+    T getById(int id);
+}
---
+++ src/fr/adaming/awal/controller/interfaces/IControllerDevice.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.controller.interfaces;
+
+import fr.adaming.awal.entity.Device;
+
+/**
+ *
+ * @author INTI0217
+ */
+public interface IControllerDevice extends IController<Device>{
+}
