1e7649218fbab6002ad388bf61f7ce2a837365f2
bianloic <bian.loic@gmail.com>
2014-09-30 15:10:55 +0200

    add Controller admin class, Controller device insurance class, Controller device repair class, Controller firm class, Controller model class, Controller model package class, Controller repairer class.

---
+++ src/fr/adaming/awal/controller/interfaces/IControllerModelPackage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.controller.interfaces;
+
+import fr.adaming.awal.entity.Modelpackage;
+
+/**
+ *
+ * @author INTI0217
+ */
+public interface IControllerModelPackage extends IController<Modelpackage>{
+    
+}
